07d7d4293e46db1114f779ff5e4f5541123824bb
Dmitry Zasenko <dzasenko@c3r7p5.42vienna.com>
2025-12-19 13:38:38 +0100

    final fix nick

--- src/commands/Nick.cpp
+++ src/commands/Nick.cpp
@@ -2,20 +2,32 @@
 
 void Server::nick(Client *c, const Command &command)
 {
+    // DEBUG
+    std::cout << "_nicks before: ";
+    for(std::map<std::string, Client *>::iterator it = _nicks.begin(); it != _nicks.end(); ++it)
+    {
+        std::cout << "["  << it->first << "] ";
+    }
+    std::cout << std::endl;
+    // END DEBUG
+
     if (!c->getPassStatus()) return;
     const std::vector<std::string> &params = command.getParams();
     if (params.size() < 1) {
         sendNumericReply(c, ERR_NONICKNAMEGIVEN, "", "");
         return;
     }
+
     const std::string &newNick = params[0];
     if (!_parser.isValidNick(newNick)) {
         sendNumericReply(c, ERR_ERRONEUSNICKNAME, newNick, "");
         return;
     }
+
     const std::string newNickLower = _parser.ircLowerStr(newNick);
     const std::string oldNick = c->getNick();
     const std::string oldNickLower = c->getNickLower();
+    
     if (oldNick.empty()) {
         if (isNickExists(newNickLower)) {
             sendNumericReply(c, ERR_NICKNAMEINUSE, newNick, "");
@@ -30,10 +42,9 @@ void Server::nick(Client *c, const Command &command)
         }
         return;
     }
-    if (oldNick == newNick) {
-        return;
-    }
 
+    if (oldNick == newNick) return;
+    
     if (oldNickLower == newNickLower) {
         c->setNick(newNick);
     }
@@ -57,16 +68,36 @@ void Server::nick(Client *c, const Command &command)
     std::set<Client *> notify;
     const std::set<std::string> &clientChannels = c->getChannels();
 
+    
     std::set<std::string>::const_iterator it = clientChannels.begin();
     for (; it != clientChannels.end(); ++it) {
+        std::cout << "user has channel: " << *it << std::endl;
+
         const std::string &chanLower = *it;
+
         std::map<std::string, Channel *>::iterator chIt = _channels.find(chanLower);
-        if (chIt == _channels.end()) continue;
+        if (chIt == _channels.end()) {
+            std::cout << "Channel not found" << std::endl;
+            continue;
+        }
         Channel *ch = chIt->second;
+
+        // DEBUG
+        std::map<std::string, Client *>::iterator it = ch->getUsers().begin();
+        std::cout << "users in channel: ";
+        for(; it != ch->getUsers().end(); ++it)
+        {
+            std::cout << "[" << it->first << "] | ";
+        }
+        std::cout << std::endl;
+        // END DEBUG
+
         if (!ch->isUser(oldNickLower)) continue;
+
         if (oldNickLower != newNickLower) {
-            ch->removeUser(oldNickLower);
             ch->addUser(c);
+            ch->removeUser(oldNickLower);
+
             if (ch->isOperator(oldNickLower)) {
                 ch->removeOperator(oldNickLower);
                 ch->addOperator(newNickLower);
@@ -80,6 +111,18 @@ void Server::nick(Client *c, const Command &command)
         for (std::map<std::string, Client *>::const_iterator u = users.begin(); u != users.end(); ++u)
             notify.insert(u->second);
     }
+    
+    // DEBUG 
+    std::cout << "Notify users: ";
+    for (std::set<Client *>::const_iterator it = notify.begin(); it != notify.end(); ++it) {
+        Client *other = *it;
+        if (other != c) {
+            std::cout << "[" << other->getNick() << "] ";
+        }
+    }
+    std::cout << std::endl;
+    // END DEBUG
+
     for (std::set<Client *>::const_iterator it = notify.begin(); it != notify.end(); ++it) {
         Client *other = *it;
         if (other != c) {
@@ -87,9 +130,21 @@ void Server::nick(Client *c, const Command &command)
             set_event_for_sending_msg(other->getFD(), true);
         }
     }
+
     if (oldNickLower != newNickLower) {
         std::map<std::string, Client *>::iterator it = _nicks.find(oldNickLower);
-        if (it == _nicks.end()) return;
+        if (it == _nicks.end()) {
+            return;
+        } 
         _nicks.erase(it);
     }
+
+    // DEBUG 
+    std::cout << "_nicks after: ";
+    for(std::map<std::string, Client *>::iterator it = _nicks.begin(); it != _nicks.end(); ++it)
+    {
+        std::cout << "["  << it->first << "] ";
+    }
+    std::cout << std::endl;
+    // END DEBUG
 }
