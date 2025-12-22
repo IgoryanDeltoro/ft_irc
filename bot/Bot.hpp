0669351ad8508ebeceed40a1d1e23591442a2ae3
IgoryanDeltoro <bondatchuk1989@gmail.com>
2025-12-22 13:51:27 +0100

    added a sinitazer and refactored

--- bot/Bot.hpp
+++ bot/Bot.hpp
@@ -1,53 +1,54 @@
 #ifndef BOT_HPP
-#define BOT_HPP
-#include <arpa/inet.h>
-#include <netinet/in.h>
-#include <sys/socket.h>
-#include <unistd.h>
-#include <cstring>
-#include <iostream>
-#include <stdlib.h>
-#include <poll.h>
-#include <cerrno>
-#include <fcntl.h>
-#include <map>
-#include <sstream>
+# define BOT_HPP
+# include <arpa/inet.h>
+# include <netinet/in.h>
+# include <sys/socket.h>
+# include <cstring>
+# include <iostream>
+# include <stdlib.h>
+# include <poll.h>
+# include <cerrno>
+# include <fcntl.h>
+# include <map>
 # include <signal.h>
 
 class Bot {
     private:
-        std::string         _ip;
-        std::string         _port;
-        std::string         _password;
-        std::string         _nick;
-        std::string         _serv_name;
+        std::string                         _ip;
+        std::string                         _port;
+        std::string                         _password;
+        std::string                         _nick;
+        std::string                         _serv_name;
+        std::string                         _recv_buffer;
+        std::string                         _send_buffer;
+        struct sockaddr_in                  _serv_addr;
+        struct pollfd                       _pfd;
+        int                                 _connected_fd;
+        int                                 _ping_time;
 
-        int                 _connected_fd;
-        struct sockaddr_in  _serv_addr;
-        std::string         _recv_buffer;
-        std::string         _send_buffer;
-        struct pollfd       _pfd;
-        int                 _ping_time;
-        int                 _ping_wind;
+        static const int                    _ping_wind = 60;
+        static const int                    _pong_recv_time = 2;
 
+        void                                invite(const std::string &);
+        void                                privmsg(const std::string &, const std::string &);
+        void                                pong(const std::string &);
+        void                                ping();
+        int                                 getsocketfd();
+        void                                handleLine(const std::string &);
+        int                                 read_income_msg();
+        void                                send_message_to_server();
+        std::map<std::string, std::string>  parse_incoming_msg (const std::string &str);
+        const std::string                   &get_param(std::map<std::string, std::string> &t, const std::string &str);
 
         Bot();
         Bot(const Bot &other);
         Bot &operator=(const Bot &other);
 
-        int getsocketfd();
-        // void sendCmd(const std::string& cmd);
-        void handleLine(const std::string &);
-        int read_income_msg();
-        void send_message_to_server();
-        std::map<std::string, std::string> parse_incomming_msg (const std::string &str);
-        const std::string &get_param(std::map<std::string, std::string> &t, const std::string &str);
-
     public:
         Bot(const std::string &, const std::string &, const std::string &);
         ~Bot();
 
-        void run();
+        void                                run();
 };
 
 #endif
